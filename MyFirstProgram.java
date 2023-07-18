f1725adc99bffcfd534d5fd8020b6eca4800b46a
Shivam <thisisshivam06@gmail.com>
2023-07-19 01:35:57 +0530

    Data types and Conditions

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -18,11 +18,30 @@
 import java.util.*;
 public class MyFirstProgram{
 public static void main(String[] args){
-  System.out.println("*****");
-  System.out.println("   * ");
-  System.out.println("  *  ");
-  System.out.println(" *   ");
-  System.out.println("*****");
-  // System.out.println("*****\n   * \n  *  \n *   \n*****");
+  // System.out.println("*****");
+  // System.out.println("   * ");
+  // System.out.println("  *  ");
+  // System.out.println(" *   ");
+  // System.out.println("*****");
+  // // System.out.println("*****\n   * \n  *  \n *   \n*****");
+
+  int x = 15;
+  int y = 10;
+  int sum = x + y;
+  System.out.println("Sum of " + x + " and "+ y + " is " + sum);
+  // xy
+  int prd = x * y;
+  System.out.println("Product of " + x + " and " + y + " is " + prd);
+  // x / y, y / x, x % y
+  int v1 = x / y;
+  int v2 = y / x;
+  int v3 = x % y;
+  System.out.println(v1);
+  System.out.println(v2);
+  System.out.println(v3);
+
+  // exp
+  int exp = ( x * y) / (x + y);
+  System.out.println(exp);
 }
 }
