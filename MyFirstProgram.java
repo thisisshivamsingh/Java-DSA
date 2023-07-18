5a93872b88f4fb22652e641cb4f46a2f9c9cee7f
Shivam <thisisshivam06@gmail.com>
2023-07-18 23:58:46 +0530

    Print Z Program

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,28 @@
+// import java.util.*;
+// public class MyFirstProgram {
+//   public static void main(String[] args){
+// // System.out.println("Hello World");
+// // System.out.print("Hello World\n");
+// // System.out.print("Hello World");
+// /////*****/////
+// // System.out.println("*");
+// // System.out.println("**");
+// // System.out.println("***");
+// // System.out.println("****");
+// // System.out.println("*****");
+// /////*****/////
+// // System.out.println("*\n**\n***\n****\n*****");                                         
+//   }  
+// }
+
 import java.util.*;
-public class MyFirstProgram {
-  public static void main(String[] args){
-    System.out.println("Hello World");
-  }  
+public class MyFirstProgram{
+public static void main(String[] args){
+  System.out.println("*****");
+  System.out.println("   * ");
+  System.out.println("  *  ");
+  System.out.println(" *   ");
+  System.out.println("*****");
+  // System.out.println("*****\n   * \n  *  \n *   \n*****");
+}
 }
